444b11a2edc88c74958617eb27ed9ccc68cd9e2a
Jonatha Botelho <lebotelhon@gmail.com>
2022-08-01 04:04:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let musica = document.querySelector('audio');
+
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('descricao i');
+
+duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+
+window.onload = duration; // O QUE É ISSO? KKKKK Não sei!
+
+// Eventos
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+musica.addEventListener('timeupdate', atualizarBarra);
+
+
+//Funções
+function tocarMusica() {
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
+}
+
+function pausarMusica() {
+    musica.pause();
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block'; 
+}
+
+function atualizarBarra() {
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
